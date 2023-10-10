9982856a4e7b31d03a7aa36e71e83a60b756ca26
stv <vksteven80@gmail.com>
2023-10-10 23:43:39 +0700

    add awal6

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:t1_2021110083/Pages/ButtonPage.dart';
-import 'package:t1_2021110083/Pages/HomePage.dart';
 
 void main() {
   runApp(const TabBarDemo());
@@ -9,23 +8,15 @@ void main() {
 class TabBarDemo extends StatelessWidget {
   const TabBarDemo({super.key});
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //       title: 'Flutter Demo',
-  //       theme: ThemeData(
-  //         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-  //         useMaterial3: true,
-  //       ),
-  //       home: const MainPage());
-  // }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'T1_2021110083',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Roboto',
+      ),
       routes: {
         "/": (context) => ButtonPage(),
       },
